0529c9c59dc9f5b664b0c73420b024d909253fee
HoiPang2 <hoipang1e06@gmail.com>
2023-12-17 21:57:52 -0500

    feat: add constants for different components

---
+++ src/app/constant/button.ts
@@ -0,0 +1,6 @@
+export const saveButtonText = "Save";
+export const updateButtonText = "Update";
+export const cancelButtonText = "Cancel";
+export const confirmButtonText = "Confirm";
+export const clickToExpand = "Click to expand";
+export const clickToCollapse = "Click to collapse";
