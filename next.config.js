ca471d0abe9bcc4959703aa890c6c8e4a604ea23
HoiPang2 <hoipang1e06@gmail.com>
2023-12-17 21:59:37 -0500

    feat: update package and project config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  /** 
+   This is for demo purposes only, you should not allow all hosts in production
+   see https://nextjs.org/docs/messages/next-image-unconfigured-host
+   you should instead add whitelisted domains to the remotePatterns array
+   e.g. aws s3 bucket, cloudinary, etc.
+  */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
